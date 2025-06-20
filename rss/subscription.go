3b662ea9b880f818802faff02306d6b44a9c50b6
Shane <develop@shenyingnan.com>
2025-06-20 15:12:58 +0800

    rss subscription

---
+++ rss/subscription.go
@@ -0,0 +1,44 @@
+package rss
+
+import (
+	"fmt"
+
+	"github.com/mmcdole/gofeed"
+)
+
+func newSubscription(url, alias string, allFilter, titleFilter, contentFilter []string) subscription {
+	return subscription{
+		url:   url,
+		alias: alias,
+		filter: filter{
+			all:     allFilter,
+			title:   titleFilter,
+			content: contentFilter,
+		},
+	}
+}
+
+type subscription struct {
+	url    string
+	alias  string
+	filter filter
+}
+
+func (s subscription) Fetch() (string, []*gofeed.Item, error) {
+	parser := newFeedParser()
+
+	feed, err := parser.ParseURL(s.url)
+	if err != nil {
+		return "", nil, fmt.Errorf("failed to get feed: %w", err)
+	}
+
+	return s.getFeedName(feed), s.filter.filter(feed.Items), nil
+}
+
+func (s subscription) getFeedName(feed *gofeed.Feed) string {
+	if s.alias != "" {
+		return s.alias
+	}
+
+	return feed.Title
+}
